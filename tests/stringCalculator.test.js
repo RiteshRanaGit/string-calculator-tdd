7fb0922125465634a98f4b0dc27cbb4d1a0de235
Ritesh Rana <rana1997ritesh@gmail.com>
2025-03-13 13:00:06 +0530

    Add another test Handle a Single Number

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -3,3 +3,7 @@ const { add } = require("../src/stringCalculator");
 test("returns 0 for an empty string", () => {
   expect(add("")).toBe(0);
 });
+
+test("returns the number itself for a single number", () => {
+    expect(add("5")).toBe(5);
+});
