073eebdaa084225eef53d90136f7180272f5b4ac
Ritesh Rana <rana1997ritesh@gmail.com>
2025-03-13 13:09:37 +0530

    Modify add

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,7 +1,15 @@
 function add(numbers) {
     if (numbers === "") return 0;
+  
+    let delimiter = /[\n,]/;
+    if (numbers.startsWith("//")) {
+      const parts = numbers.split("\n");
+      delimiter = parts[0].substring(2);
+      numbers = parts[1];
+    }
+  
     return numbers
-      .split(/[\n,]/)
+      .split(new RegExp(delimiter))
       .reduce((sum, num) => sum + parseInt(num), 0);
   }
 module.exports = { add };
