bddd44daf288df945e61d7ebf565962833923163
Ritesh Rana <rana1997ritesh@gmail.com>
2025-03-13 13:05:02 +0530

    Handle Two Numbers Separated by a Comma

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -7,3 +7,7 @@ test("returns 0 for an empty string", () => {
 test("returns the number itself for a single number", () => {
     expect(add("5")).toBe(5);
 });
+
+test("returns the sum of two numbers", () => {
+    expect(add("1,5")).toBe(6);
+});
